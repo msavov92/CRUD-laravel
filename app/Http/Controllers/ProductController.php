dff2160110212b9ea5a237610db851f69b00aa67
msavov92 <msavov92@gmail.com>
2023-08-03 01:02:02 +0300

    init repo

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('products.index');
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:2',
+            'description' => 'nullable'
+        ]);
+
+        $newProduct = Product::create($data);
+
+        return redirect(route('product.index'));
+    }
+}
